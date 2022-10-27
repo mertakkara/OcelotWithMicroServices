afaa8ff5ebbcd2e063069a6e224cb93381d78527
Mert AKYILDIZ <mertakkara@hotmail.com>
2022-10-27 20:11:14 +0300

    final commit

---
+++ ProductMicroservice/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductMicroservice.Model;
+using ProductMicroservice.Services.Interface;
+
+namespace ProductMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService productService;
+        public ProductsController(IProductService _productService)
+        {
+            productService = _productService;
+        }
+        [HttpGet]
+        public IEnumerable<Product> ProductList()
+        {
+           Product product = new Product();
+            product.ProductDescription = "a";
+            product.ProductName = "a";
+            product.ProductPrice = 2;
+            product.ProductId = 2;
+            product.ProductStock = 2;
+            return (IEnumerable<Product>)product;
+
+        }
+    }
+}
---
+++ UserMicroservice/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserMicroservice.Model;
+using UserMicroservice.Services.Interface;
+
+namespace UserMicroservice.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UsersController(IUserService _userService)
+        {
+            userService = _userService;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> UserList()
+        {
+            User product = new User();
+            product.Address = "a";
+            product.UserId = 2;
+            product.UserName = "aa";
+            return (IEnumerable<User>)product;
+        }
+    }
+}
